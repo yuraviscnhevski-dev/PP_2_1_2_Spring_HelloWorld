bb7fee602fd4a7ed6cc3873bfacea3df3ff13e9d
yuraviscnhevski-dev <yura.viscnhevski@gmail.com .>
2026-02-10 19:16:38 +0300

    fix pom

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,30 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
+
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        System.out.println("HelloWorld bean: " + bean.getMessage());
+
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println("HelloWorld bean1: " + bean1.getMessage());  // Исправлено!
+
+
+        // Получаем ДВА раза бин Cat (singleton)
+        Cat bean2 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println("Cat bean2: " + bean2.getMessage());  // Исправлено!
+
+        Cat bean3 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println("Cat bean3: " + bean3.getMessage());  // Исправлено!
+
+        // Сравниваем ссылки
+        System.out.println("\n=== Сравнение ссылок ===");
+        System.out.println("HelloWorld bean == HelloWorld bean1? " + (bean == bean1));
+        System.out.println("Cat bean2 == Cat bean3? " + (bean2 == bean3));
+
+
     }
 }
