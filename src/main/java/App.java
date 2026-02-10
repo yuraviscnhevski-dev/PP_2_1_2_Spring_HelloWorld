1971d37185877e10ee5c821517e507c533203c77
yuraviscnhevski-dev <yura.viscnhevski@gmail.com .>
2026-02-10 15:23:22 +0300

    fix pom

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,6 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        System.out.println("привет");
     }
 }
